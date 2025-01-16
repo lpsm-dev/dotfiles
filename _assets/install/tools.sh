475c2e123fb808155a1ae02afd8c54f9cadd9214
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-01-15 23:08:22 -0300

    fix: change folder

--- _assets/install/tools.sh
+++ _assets/install/tools.sh
@@ -1,3 +1,2 @@
-# 🌺 Minimalist Vim Plugin Manager
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
