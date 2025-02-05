be254097683bcb74aa6d920fa5bcbee05d11b221
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-04 23:36:56 -0300

    fix: setup bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,30 +3,22 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Variables
+
 HOMEBREW_INSTALL_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
 
-choice="Y"
+# Functions
 
-if ! command -v brew &> /dev/null; then
-    read -p "Brew not found. Do you want to install it? (Y/n) " -n 1 -r choice
-    echo
+is_command_not_in_path() { ! [ -x "$(command -v "$1")" ]; }
+
+# Main
+
+if is_command_not_in_path brew; then
+	echo 'Installing brew...'
+	/bin/bash -c "$(curl -fsSL $HOMEBREW_INSTALL_URL"
 fi
 
-case "$choice" in
-    y|Y )
-        /bin/bash -c "$(curl -fsSL $HOMEBREW_INSTALL_URL)" 
-        if [ $? -ne 0 ]; then
-            echo "Homebrew installation failed. Exiting..."
-            exit 1
-        fi
-        ;;
-    n|N )
-        echo "Please install Homebrew first."
-        exit 1
-        ;;
-    * ) echo "Invalid input. Please try again." ;;
-esac
-
 brew update
+brew upgrade
 
 echo "Dotfiles setup complete!"
