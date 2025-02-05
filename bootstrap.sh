5c445e2dc1fa617a67edcca70ac7f0d90598ef4b
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-05 02:39:01 -0300

    fix: code vars

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,9 @@ GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 RESET="\033[0m"
 
+GIT_DOTFILES_URL="https://github.com/lpsm-dev/dotfiles.git"
+DEFAULT_DOTFILES_PATH="~/.dotfiles"
+
 # ==============================================
 # FUNCTIONS
 # ==============================================
@@ -46,16 +49,16 @@ function is_command_not_in_path() { ! [ -x "$(command -v "$1")" ]; }
 # MAIN
 # ==============================================
 
-os_name="$(uname -s)"
-case "${os_name}" in
-Darwin*)
+OS=$(uname -s)
+case $OS in
+Darwin)
     info "Setting up for macOS"
-    git clone --depth=1 https://github.com/lpsm-dev/dotfiles.git ~/.dotfiles
+	if [ ! -d "$DEFAULT_DOTFILES_PATH" ]; then
+		info "Cloning $GIT_DOTFILES_URL to $DEFAULT_DOTFILES_PATH"
+		git clone --depth=1 $GIT_DOTFILES_URL $DEFAULT_DOTFILES_PATH
+	fi
     ;;
-*)
-    error "Unsupported OS: ${os_name}"
-    exit 1
-    ;;
+*) error "Unsupported OS: ${os_name}" && exit 1 ;;
 esac
 
 info "> Dotfiles setup complete!"
