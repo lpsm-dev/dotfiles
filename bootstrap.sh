9fe864721a52640b78bf296ed9eede5fed015205
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-05 12:07:07 -0300

    fix: change code

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,7 +92,6 @@ function setup_brew() {
 		info "Instalando Homebrew..."
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         if [[ "$(uname -m)" == "arm64" ]]; then
-            echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
             eval "$(/opt/homebrew/bin/brew shellenv)"
         fi
 	else
