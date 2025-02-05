fb0c7319c337690e34d7ca3f7aa3bb49f6453742
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-05 02:55:07 -0300

    fix: code project

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,6 +45,22 @@ function error {
 
 function is_command_not_in_path() { ! [ -x "$(command -v "$1")" ]; }
 
+function setup_macos() {
+	if [ "$(id -u)" -eq 0 ]; then
+		bash "$DEFAULT_DOTFILES_PATH/terminal/.macos"
+	else
+		error "User sem permissão de administrador. Tente novamente..." && exit 1
+	fi	
+}
+
+function setup_brew() {
+	if is_command_not_in_path brew; then
+		info "Instalando brew..."
+		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+	fi
+	brew bundle --file="$DEFAULT_DOTFILES_PATH/terminal/Brewfile"
+}
+
 # ==============================================
 # MAIN
 # ==============================================
@@ -54,25 +70,15 @@ case $OS in
 Darwin)
     info "Setting up for macOS"
 	if [ ! -d "$DEFAULT_DOTFILES_PATH" ]; then
-		# Setup Dotfiles Project
+		# Setup Git Project
 		info "Cloning $GIT_DOTFILES_URL to $DEFAULT_DOTFILES_PATH"
 		git clone --depth=1 $GIT_DOTFILES_URL $DEFAULT_DOTFILES_PATH
 
 		# Setup MacOs
-		if [ "$(id -u)" -eq 0 ]; then
-			bash "$DEFAULT_DOTFILES_PATH/terminal/.macos"
-		else
-			error "User sem permissão de administrador. Tente novamente..." && exit 1
-		fi
+		setup_macos
 
 		# Setup Brew
-		if is_command_not_in_path brew; then
-			info "Instalando brew..."
-			curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
-		fi
-
-		# Setup Brew Dependencies
-		brew bundle --file="$DEFAULT_DOTFILES_PATH/terminal/Brewfile"
+		setup_brew
 	fi
     ;;
 *) error "Unsupported OS: ${os_name}" && exit 1 ;;
