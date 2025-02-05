13862f095dc27013ee88e793d7e22e039d033286
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-05 02:53:13 -0300

    fix: setup commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,17 +19,9 @@ DEFAULT_DOTFILES_PATH="~/.dotfiles"
 # FUNCTIONS
 # ==============================================
 
-function error {
-	if [ -t 0 ] && [ -t 1 ]; then
-		echo -e "${RED}$1${RESET}"
-	else
-		echo "$1"
-	fi
-}
-
 function info {
 	if [ -t 0 ] && [ -t 1 ]; then
-		echo -e "${GREEN}$1${RESET}"
+		echo -e "${GREEN}╰┈➤ $1${RESET}"
 	else
 		echo "$1"
 	fi
@@ -37,7 +29,15 @@ function info {
 
 function warn {
 	if [ -t 0 ] && [ -t 1 ]; then
-		echo -e "${YELLOW}$1${RESET}"
+		echo -e "${YELLOW}╰┈➤ $1${RESET}"
+	else
+		echo "$1"
+	fi
+}
+
+function error {
+	if [ -t 0 ] && [ -t 1 ]; then
+		echo -e "${RED}╰┈➤ $1${RESET}"
 	else
 		echo "$1"
 	fi
@@ -54,11 +54,28 @@ case $OS in
 Darwin)
     info "Setting up for macOS"
 	if [ ! -d "$DEFAULT_DOTFILES_PATH" ]; then
+		# Setup Dotfiles Project
 		info "Cloning $GIT_DOTFILES_URL to $DEFAULT_DOTFILES_PATH"
 		git clone --depth=1 $GIT_DOTFILES_URL $DEFAULT_DOTFILES_PATH
+
+		# Setup MacOs
+		if [ "$(id -u)" -eq 0 ]; then
+			bash "$DEFAULT_DOTFILES_PATH/terminal/.macos"
+		else
+			error "User sem permissão de administrador. Tente novamente..." && exit 1
+		fi
+
+		# Setup Brew
+		if is_command_not_in_path brew; then
+			info "Instalando brew..."
+			curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+		fi
+
+		# Setup Brew Dependencies
+		brew bundle --file="$DEFAULT_DOTFILES_PATH/terminal/Brewfile"
 	fi
     ;;
 *) error "Unsupported OS: ${os_name}" && exit 1 ;;
 esac
 
-info "> Dotfiles setup complete!"
+info "Setup Dotfiles finalizado!"
