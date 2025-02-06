a529e638e19ee4a51e1d891c81867fd97de572a9
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-06 01:31:21 -0300

    fix: upgrade config

--- bootstrap.sh
+++ bootstrap.sh
@@ -172,7 +172,7 @@ function setup_automations(){
 # MAIN
 # ==============================================
 
-VERSION="0.0.5"
+VERSION="0.0.6"
 
 info "Hey Folks! Welcome to bootstrap MacOs! - $VERSION"
 sudo -v
