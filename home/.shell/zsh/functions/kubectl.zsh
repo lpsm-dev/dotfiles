a2d89252faafed93cf30d976de6356d499dbdfcf
lpsm-dev <lpsm-dev@protonmail.com>
2025-02-06 01:57:58 -0300

    feat: add functions

--- home/.shell/zsh/functions/kubectl.zsh
+++ home/.shell/zsh/functions/kubectl.zsh
@@ -8,3 +8,15 @@
 # ██║  ██╗╚██████╔╝██████╔╝███████╗╚██████╗   ██║   ███████╗
 # ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝   ╚═╝   ╚══════╝
 #                                                          
+
+function kdebug() {
+    kubectl run -i --rm --tty debug --image=busybox --restart=Never -- sh
+}
+
+function kadd(){
+    if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Os params para o kubeconfig file and kubeconfig context name devem ser preenchidos..."
+    else
+        kubectl kc add --file="$1" --context-name="$2"
+    fi
+}
