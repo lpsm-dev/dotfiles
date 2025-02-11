5994f511458bf90203f272eb317ed3f94808110f
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-11 03:00:45 -0300

    chore(sorry): just dummy changes - 7CB5B864

--- bootstrap.sh
+++ bootstrap.sh
@@ -127,7 +127,9 @@ function setup_terminal(){
         warn "Devbox is already installed"
     fi
     info "Install neovim plugins"
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ] || \
+      sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
+        --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
 function setup_ai_tools(){
