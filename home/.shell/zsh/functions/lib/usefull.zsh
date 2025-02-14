5b4aab5625b536f9bdcca762a9b5632da49036f7
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-14 01:02:10 -0300

    fix: setup libs

--- home/.shell/zsh/functions/lib/usefull.zsh
+++ home/.shell/zsh/functions/lib/usefull.zsh
@@ -102,3 +102,12 @@ brewclear() {
     ;;
   esac
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
