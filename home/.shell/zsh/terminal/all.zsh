6c05af9dded9eed3b955ed988667db3c003f4981
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-14 00:47:29 -0300

    fix: rm duplicate

--- home/.shell/zsh/terminal/all.zsh
+++ home/.shell/zsh/terminal/all.zsh
@@ -12,6 +12,7 @@
 # ================================================
 # LOADING FILES
 # ================================================
-[ -f "$HOME/.shell/zsh/terminal/lib/options.zsh" ]    && source $HOME/.shell/zsh/terminal/lib/options.zsh
-[ -f "$HOME/.shell/zsh/terminal/lib/terminal.zsh" ]  && source $HOME/.shell/zsh/terminal/lib/terminal.zsh
-[ -f "$HOME/.shell/zsh/terminal/lib/zinit.zsh" ]      && source $HOME/.shell/zsh/terminal/lib/zinit.zsh
+[ -f "$HOME/.shell/zsh/terminal/lib/options.zsh" ]      && source $HOME/.shell/zsh/terminal/lib/options.zsh
+[ -f "$HOME/.shell/zsh/terminal/lib/programming.zsh" ]  && source $HOME/.shell/zsh/terminal/lib/programming.zsh
+[ -f "$HOME/.shell/zsh/terminal/lib/terminal.zsh" ]     && source $HOME/.shell/zsh/terminal/lib/terminal.zsh
+[ -f "$HOME/.shell/zsh/terminal/lib/zinit.zsh" ]        && source $HOME/.shell/zsh/terminal/lib/zinit.zsh
