4d0e1d2fa2a4841ff00ef49e80718a28d8bd03f7
lpsm-dev-github <lpsm-dev@protonmail.com>
2025-02-14 15:29:06 -0300

    feat: add git submodules

--- home/.shell/zsh/exports/lib/programming.zsh
+++ home/.shell/zsh/exports/lib/programming.zsh
@@ -18,8 +18,8 @@ export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 # NODE
 # ================================================
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 
 # ================================================
 # GO
