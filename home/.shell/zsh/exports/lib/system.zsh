440c46796ea80b973425e9a1b1b9505a9a569a60
lpsm-dev <lpsm-dev@protonmail.com>
2025-03-10 23:04:26 -0300

    chore: organize code

--- home/.shell/zsh/exports/lib/system.zsh
+++ home/.shell/zsh/exports/lib/system.zsh
@@ -16,6 +16,17 @@ export SLS_TELEMETRY_DISABLED=1
 export SLS_TRACKING_DISABLED=1
 
 # ================================================
+# BITWARDEN
+# ================================================
+export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
+export BITWARDEN_SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
+
+# ================================================
+# GPG
+# ================================================
+export GPG_TTY=$(tty)
+
+# ================================================
 # HOMEBREW
 # ================================================
 export HOMEBREW_NO_ANALYTICS=1
@@ -30,14 +41,3 @@ export HOMEBREW_BREWFILE=~/Brewfile
 export LANG="en_US"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-
-# ================================================
-# GPG
-# ================================================
-export GPG_TTY=$(tty)
-
-# ================================================
-# BITWARDEN
-# ================================================
-export SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
-export BITWARDEN_SSH_AUTH_SOCK=$HOME/.bitwarden-ssh-agent.sock
