bfd6e51df58493eacd9a3ef72d5a9ec2c6695af0
lpsm-dev <lpsm-dev@protonmail.com>
2025-07-09 02:20:25 -0300

    refactor(yazi): Use builtin cd and remove function definition

--- home/.shell/zsh/functions/lib/usefull.zsh
+++ home/.shell/zsh/functions/lib/usefull.zsh
@@ -103,11 +103,11 @@ brewclear() {
   esac
 }
 
-function y() {
-    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-    yazi "$@" --cwd-file="$tmp"
-    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-        builtin cd -- "$cwd"
-    fi
-    rm -f -- "$tmp"
+y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
